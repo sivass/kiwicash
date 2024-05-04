2f3cc3f6b5e321df3513d1635bdadf2d74d91351
sivass <sivass2703@gmail.com>
2024-05-04 17:09:57 +0800

    implement headebox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Siva'};
   return (
-    <div>page</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || 'title'} subtext="Access and manage you account and transaction efficiently"  />
+            </header>
+        </div>
+    </section>
   )
 }
 
